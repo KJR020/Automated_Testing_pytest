5524cc5ab63cf0aa837bd64368609ef32f297df8
Kojiro <johnjiro1114@gmail.com>
2022-12-22 13:44:12 +0000

    unittest assertEqual blog.py

---
+++ section3/blog.py
@@ -0,0 +1,14 @@
+class Blog:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.posts = []
+
+    def __repr__(self):
+        return f"{self.title} by {self.author} ({len(self.posts)} post{'s' if len(self.posts) != 1 else ''})"
+
+    def create_post(self, title, content):
+        pass
+
+    def json(self):
+        pass
