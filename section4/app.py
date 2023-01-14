1337fbfd1d54ac82574471a08978a14b9ca4772c
Kojiro <johnjiro1114@gmail.com>
2023-01-14 12:51:45 +0000

    get_dataのテスト

--- section4/app.py
+++ section4/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")  # http://www.mysite.com/
 def home():
-    return jsonify({"message": "Hello World!"})
+    return jsonify({"message": "Hello, World!"})
 
 
 if __name__ == "__main__":
